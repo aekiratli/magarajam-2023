8440204dac35a97d444c1581bd460939cff82910
aekiratli <emre719@gmail.com>
2023-11-18 00:02:55 +0300

    removed print lines

--- Game/Features/Relics/Scripts/RelicGenerator.gd
+++ Game/Features/Relics/Scripts/RelicGenerator.gd
@@ -11,10 +11,6 @@ func _ready():
 	var width = MapController.width
 	var height = MapController.height
 
-	var arr = [Vector2(0, 0), Vector2(0, 1), Vector2(1, 0), Vector2(1, 1)]
-	if Vector2(0,0) in arr:
-		print("yes")
-
 	await get_tree().create_timer(0.000001).timeout
 	var starting_area_coordinates = MapController.MapGenerator.starting_area_coordinates
 	for i in range(RelicController.number_of_relics):
@@ -24,13 +20,12 @@ func _ready():
 			var spawn_tile_pos = Vector2(randi() % width, randi() % height)
 			var spawn_tile_index = spawn_tile_pos.x + spawn_tile_pos.y * width
 			var is_in_starting_area = false
+
 			for coords in starting_area_coordinates:
 				if spawn_tile_pos.x == coords.x and spawn_tile_pos.y == coords.y:
-					print("in starting area")
 					is_in_starting_area = true
+
 			if map[spawn_tile_index] == 1 and not is_in_starting_area:
-				print(spawn_tile_pos)
 				is_valid = true
 				var spawn_global_pos = MapController.map_to_local(spawn_tile_pos)
 				RelicController.create_relic(spawn_global_pos)
-	print(starting_area_coordinates)
